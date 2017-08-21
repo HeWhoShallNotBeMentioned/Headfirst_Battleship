8f7f176734c6d9fb7b4f00f09f224e2927313445
Chris Underwood <cunderwoodmn@gmail.com>
2017-08-20 19:27:54 -0700

    entered code to randomly generate ships

--- app.js
+++ app.js
@@ -19,51 +19,84 @@ var view = {
 };
 
 var model = {
-boardSize: 7,
-numShips: 3,
-shipLength: 3,
-shipsSunk: 0,
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
 
-ships: [{
-  locations: ["06", "16", "26"], hits: ["","",""]
-},
+  ships: [{
+    locations: ["06", "16", "26"], hits: ["","",""]
+  },
 
-{
-  locations: ["24", "34", "44"], hits: ["","",""]
-},
- {
-  locations: ["10", "11", "12"], hits: ["","",""]
-}],
+  {
+    locations: ["24", "34", "44"], hits: ["","",""]
+  },
+   {
+    locations: ["10", "11", "12"], hits: ["","",""]
+  }],
 
-fire: function (guess) {
+  fire: function (guess) {
 
-  for (var j = 0; j < this.numShips; j++) {
-    var ship = this.ships[j];
-    var index = ship.locations.indexOf(guess);
-    if (index >= 0){
-      ship.hits[index] = "hit";
-      view.displayHit(guess);
-      view.displayMessage("Hit!");
-      if(this.isSunk(ship)) {
-        view.displayMessage("You sank my Battleship!");
-        this.shipsSunk++;
+    for (var j = 0; j < this.numShips; j++) {
+      var ship = this.ships[j];
+      var index = ship.locations.indexOf(guess);
+      if (index >= 0){
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("Hit!");
+        if(this.isSunk(ship)) {
+          view.displayMessage("You sank my Battleship!");
+          this.shipsSunk++;
+        }
+        return true;
       }
-      return true;
     }
-  }
-  view.displayMiss(guess);
-  view.displayMessage("You missed. Haaa Haaaa!");
-  return false;
-},
+    view.displayMiss(guess);
+    view.displayMessage("You missed. Haaa Haaaa!");
+    return false;
+  },
 
-isSunk: function (ship) {
-  for (var j = 0; j < this.shipLength; j++) {
-    if (ship.hits[j] !== "hit"){
-      return false;
+  isSunk: function (ship) {
+    for (var j = 0; j < this.shipLength; j++) {
+      if (ship.hits[j] !== "hit"){
+        return false;
+      }
     }
+    return true;
+  },
+
+  generateShipLocations: function(){
+    var locations;
+    for (var j = 0; j < this.numShips; j++){
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+      this.ships(j).locations = locations;
+    }
+  },
+
+  generateShip: function () {
+    var direction = Math.floor(Math.random() * 2);
+    var row;
+    var col;
+    if (direction === 1) {
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * this.boardSize - (this.shipLength + 1));
+    } else {
+      col = Math.floor(Math.random() * this.boardSize);
+      row = Math.floor(Math.random() * this.boardSize - (this.shipLength + 1));
+    }
+
+    var newShipLocations = [];
+    for (var j = 0; j < this.shipLength; j++) {
+      if (direction === 1) {
+        newShipLocations.push(row + "" + (col + j));
+      } else {
+        newShipLocations.push((row + j) + "" + col);
+      }
+    }
+    return newShipLocations;
   }
-  return true;
-}
 
 };
 
